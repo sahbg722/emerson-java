d4ff97dbdfacc20b3c79948d1ec010acacc2d032
sahbg722 <144184809+sahbg722@users.noreply.github.com>
2024-07-03 20:31:23 -0300

    Add files via upload

---
+++ Exemplo.java
@@ -0,0 +1,8 @@
+public class Exemplo {
+   public static void main(String[] args) {
+    int base = 5;
+    int largura = 10;
+    int area = base*largura;
+    System.out.println("a area da sala é:" + area);
+   } 
+}
